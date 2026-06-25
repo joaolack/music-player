50011787b929f2dcb9c9d2c58ec120f741cb082b
joaolack <jaoglack@proton.me>
2026-06-25 16:46:59 -0300

    add: javascript

---
+++ index.js
@@ -0,0 +1,5 @@
+const songName = document.getElementById("song-name");
+const artistName = document.getElementById("artist-name");
+
+songName.innerText = "Slide Away - Remastered";
+artistName.innerText = "Oasis";
